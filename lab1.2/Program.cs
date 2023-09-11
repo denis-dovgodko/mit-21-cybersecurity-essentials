2de3148a65888e1a73345f90615badd5a88e03a5
martine <dendov555@gmail.com>
2023-09-11 14:06:02 +0300

    1.1

---
+++ lab1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rndNumberGenerator = new RNGCryptoServiceProvider();
+            int length = 10;
+            var randomNumber = new byte[length];
+            rndNumberGenerator.GetBytes(randomNumber);
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(randomNumber[i]);
+            }
+        }
+    }
+}
