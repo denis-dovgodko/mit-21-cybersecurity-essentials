10247a26e8fa7e4c6bedc9038cf17592add62c60
martine <dendov555@gmail.com>
2023-09-12 09:46:35 +0300

    practice1

---
+++ lab1/lab1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(673);
+            for(int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(random.Next(1,10));
+            }
+        }
+    }
+}
