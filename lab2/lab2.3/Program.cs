b7ee56f9ed19215ed5d470aa3d5de05d7f42546a
martine <dendov555@gmail.com>
2023-09-13 20:56:22 +0300

    2.3

--- lab2/lab2.3/Program.cs
+++ lab2/lab2.3/Program.cs
@@ -16,32 +16,50 @@ namespace lab2._1
             int len = decData.Length;
             string data = "Mit21";
             byte[] search = Encoding.UTF8.GetBytes(data);
-            byte[] decrypMes = new byte[len];
-            byte[] decryptKey = new byte[len];
-            int bytes = 0;
-            bytes = Find(search, decData, len, decrypMes,bytes,decryptKey);
-            while (bytes != decData.Length)
+            for (int begin = 0; begin < 52; begin++)
             {
-                bytes = Find(search, decData, len, decrypMes,bytes, decryptKey);
+                byte[] decrypMes = new byte[len];
+                byte[] decryptKey = new byte[len];
+                int bytes = 0;
+                bytes = Find(search, decData, len, begin, decrypMes, bytes, decryptKey);
+                while (bytes != 5)
+                {
+                    bytes = Find(search, decData, len, begin, decrypMes, bytes, decryptKey);
+                }
+                for (int i = begin + 5; i < 57; i++)
+                {
+                    int s = i - 5;
+                    decrypMes[i] = (byte)(decData[i] ^ decryptKey[s]);
+                    decryptKey[i] = decryptKey[s];
+                }
+                if(begin!=0) {
+                    for (int i = begin - 1; i >= 0; i--)
+                    {
+                        int s = i + 5;
+                        decrypMes[i] = (byte)(decData[i] ^ decryptKey[s]);
+                        decryptKey[i] = decryptKey[s];
+                    }
+                }
+                System.Console.WriteLine("Code: ");
+                foreach (byte b in decryptKey)
+                {
+                    Console.Write(b.ToString("D") + " ");
+                }
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+                System.Console.WriteLine("Text of .dat file: ");
+                string encrypted = Encoding.UTF8.GetString(decrypMes);
+                System.Console.WriteLine(encrypted);
+                System.Console.WriteLine();
             }
-            Console.OutputEncoding = Encoding.UTF8;
-            System.Console.WriteLine("Code: ");
-            foreach (byte b in decryptKey)
-            {
-                Console.Write(b.ToString("D") + " ");
-            }
-            System.Console.WriteLine();
-            System.Console.WriteLine("Text of .dat file: ");
-            string encrypted = Encoding.UTF8.GetString(decrypMes);
-            System.Console.WriteLine(encrypted);
         }
 
-        static int Find(byte[] search, byte[] decData, int len, byte[] decrypMes, int bytes, byte[] decryptKey)
+        static int Find(byte[] search, byte[] decData, int len, int begin, byte[] decrypMes, int bytes, byte[] decryptKey)
         {
             byte[] key = CreateKey(len);
-            for (int i = 0, s = 0; i < 57; i++)
+            for (int i = begin, s = 0; i < begin+5; i++)
             {
-                s = i % 5;
+                s = i-begin;
                 if ((byte)(decData[i] ^ key[i]) == search[s] && decrypMes[i] ==0 && decryptKey[i] ==0)
                 {
                     decrypMes[i] = (byte)(decData[i] ^ key[i]);
