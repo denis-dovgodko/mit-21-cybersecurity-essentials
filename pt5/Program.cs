efda5a530eb9cee4669ccd2ad7f70140fa6734ac
martine <dendov555@gmail.com>
2023-10-30 19:31:37 +0200

    added .gitignore

---
+++ pt5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pt5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
